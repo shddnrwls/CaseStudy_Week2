ff6cc1e2f3c2dab5edd259044faf309908a27d17
shddnrwls <shddnrwls@gmail.com>
2022-07-09 13:13:16 +0900

    Initial Commit

---
+++ CS_Week2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS_Week2
+//
+//  Created by 노욱진 on 2022/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
